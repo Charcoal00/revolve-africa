5c4e5c45986888837803c783dd7bac200e1507e7
Charcoal00 <wisestar175@gmail.com>
2025-03-22 21:49:06 +0100

    fifth commit

--- routes/auth.js
+++ routes/auth.js
@@ -36,16 +36,24 @@ router.post("/login", async (req, res) => {
     try {
         const { email, password } = req.body;
         const user = await User.findOne({ email });
-        if (!user || !(await bcrypt.compare(password, user.password))) {
-            return res.status(401).json({ message: "Invalid credentials" });
-        }
+        if (!user) return res.status(404).json({ message: "User not found" });
 
-        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid)
+            return res
+                .status(401)
+                .json({ message: "Invalid Username or Password" });
+
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
             expiresIn: "1h",
         });
-        res.json({ token });
-    } catch {
-        res.status(500).json({ error: "Login failed" });
+        res.json({
+            message: "Login successful",
+            token,
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal server error" });
     }
 });
 
